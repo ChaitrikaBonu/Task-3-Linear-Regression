6140370e38f7f1727006ae8576ffa37e97178b67
ChaitrikaBonu <chaitrikabonu@gmail.com>
2026-06-02 15:04:07 +0530

    Update task3.py

--- task3.py
+++ task3.py
@@ -1,1 +1,74 @@
+#importing
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import kagglehub
+import os
 
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import r2_score
+
+#loadingDS
+path = kagglehub.dataset_download("brendan45774/test-file")
+csv_file = [f for f in os.listdir(path) if f.endswith('.csv')][0]
+df = pd.read_csv(os.path.join(path, csv_file))
+print(df.head())
+
+#dataProcessing
+df['Age'] = df['Age'].fillna(df['Age'].median())
+df['Fare'] = df['Fare'].fillna(df['Fare'].median())
+X = df[['Age', 'Pclass']]
+y = df['Fare']
+
+#splittingDS
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.2,
+    random_state=42
+)
+
+#training
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+#testing
+y_pred = model.predict(X_test)
+
+#evaluationOfModel
+mae = mean_absolute_error(y_test, y_pred)
+mse = mean_squared_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+print("MAE :", mae)
+print("MSE :", mse)
+print("R² Score :", r2)
+
+#modelCoefficients
+print("Intercept:", model.intercept_)
+print("Coefficients:")
+for feature, coef in zip(X.columns, model.coef_):
+    print(feature, ":", coef)
+
+#visualization
+plt.figure(figsize=(8,5))
+plt.scatter(y_test, y_pred)
+plt.xlabel("Actual Fare")
+plt.ylabel("Predicted Fare")
+plt.title("Actual vs Predicted Fare")
+plt.savefig("actual_vs_predicted.png", dpi=300)
+plt.show()
+
+#residualPlots
+residuals = y_test - y_pred
+plt.figure(figsize=(8,5))
+plt.scatter(y_pred, residuals)
+plt.axhline(y=0)
+plt.xlabel("Predicted Fare")
+plt.ylabel("Residuals")
+plt.title("Residual Plot")
+plt.savefig("residual_plot.png", dpi=300)
+plt.show()
